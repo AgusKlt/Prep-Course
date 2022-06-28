110a668ec95fe314d4af507fa1c843c46cd7a64c
AgusKlt <akaltmayer@gmail.com>
2022-06-28 17:48:04 -0300

    Fifth commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array [0]
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return (array)[array.length - 1]
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length
 }
 
 
@@ -23,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var newarray = []
+  for (var index = 0; index < array.length; index++) {
+    newarray[index] = array[index] + 1; 
+  }
+  return newarray
 }
 
 
@@ -30,6 +38,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push (elemento)
+  return array
 }
 
 
@@ -38,6 +48,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift (elemento)
+  return array
 }
 
 
@@ -47,6 +59,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(" ")
 }
 
 
@@ -54,6 +67,12 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === elemento) {
+      return true
+    }
+  }
+  return false
 }
 
 
@@ -61,6 +80,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0
+    for (var i = 0; i < numeros.length; i++) {
+      suma = suma + numeros[i];
+    }
+  return suma
 }
 
 
@@ -68,6 +92,13 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var sumaTotal = 0
+  var numeroTest = resultadosTest.length
+  for (i = 0; i < resultadosTest.length; i++){
+      sumaTotal += resultadosTest[i]
+    }
+  var promedio = sumaTotal / numeroTest
+  return promedio
 }
 
 
@@ -75,6 +106,12 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var addi = 0
+  for (i = 0; i < numeros.length; i++) {
+    if (numeros[i] > addi)
+    addi = numeros[i]
+  } 
+  return addi
 }
 
 
@@ -82,6 +119,16 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+
+  var producto = 1
+  if (arguments.length === 0) return 0;
+  else {
+    for (let i = 0; i < arguments.length; i++) {
+      producto *= arguments[i];
+    }
+    return producto
+  }
+
 }
 
 
@@ -89,6 +136,12 @@ function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
 
+  var cantidadElem = 0
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] > 18)
+    cantidadElem = cantidadElem + 1
+  }
+  return cantidadElem
 }
 
 
@@ -96,8 +149,26 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí
+  switch (numeroDeDia) {
+    case 1:
+      return "Es fin de semana"
+    case 2:
+      return "Es dia Laboral"
+    case 3:
+      return "Es dia Laboral"
+    case 4:
+      return "Es dia Laboral"
+    case 5:
+      return "Es dia Laboral"
+    case 6:
+      return "Es dia Laboral"
+    case 7:
+      return "Es fin de semana"
+
+    default:
+      break;
+  }
 } 
 
 
